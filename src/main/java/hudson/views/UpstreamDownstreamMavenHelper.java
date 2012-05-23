a55fc0addb2ebb37b53c8c7c9bdd3caf11ffa0f2
jacob_robertson <jacob_robertson@dev.java.net>
2012-05-23 16:31:26 +0000

    removing debug logging

--- src/main/java/hudson/views/UpstreamDownstreamMavenHelper.java
+++ src/main/java/hudson/views/UpstreamDownstreamMavenHelper.java
@@ -1,20 +1,16 @@
 package hudson.views;
 
-import java.util.List;
-import java.util.logging.Logger;
-
 import hudson.maven.MavenModuleSet;
 import hudson.model.AbstractProject;
 import hudson.model.TopLevelItem;
 import hudson.views.PluginHelperUtils.PluginHelperTestable;
 
-public class UpstreamDownstreamMavenHelper implements PluginHelperTestable {
+import java.util.List;
 
-	private static final transient Logger log = Logger.getLogger("updownmaven");
+public class UpstreamDownstreamMavenHelper implements PluginHelperTestable {
 
 	@SuppressWarnings("unchecked")
 	public boolean isFirstUpstreamFromSecond(TopLevelItem first, TopLevelItem second) {
-		log.warning("buildMavenHelper.isFirstUpstreamFromSecond." + first.getName() + "." + second.getName());
 		if (first instanceof MavenModuleSet && second instanceof MavenModuleSet) {
 			MavenModuleSet m2 = (MavenModuleSet) second;
 			List<AbstractProject> upstream = m2.getUpstreamProjects();
