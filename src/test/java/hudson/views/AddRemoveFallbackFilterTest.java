d69e35813a79142e67d313704e0dd5b451c3681c
Sven Schoenung <sven.schoenung@gmail.com>
2018-09-14 21:32:34 +0200

    Increave coverage in AddRemoveFallbackFilterTest

--- src/test/java/hudson/views/AddRemoveFallbackFilterTest.java
+++ src/test/java/hudson/views/AddRemoveFallbackFilterTest.java
@@ -1,66 +1,127 @@
 package hudson.views;
 
+import hudson.model.ListView;
 import hudson.model.TopLevelItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.common.collect.Lists;
 import org.junit.Before;
 import org.junit.Test;
 
-import static hudson.views.AddRemoveFallbackFilter.FallbackTypes;
+import static com.google.common.collect.Lists.newArrayList;
+import static hudson.views.AddRemoveFallbackFilter.FallbackTypes.*;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class AddRemoveFallbackFilterTest extends AbstractHudsonTest {
 
-	private AddRemoveFallbackFilter filter;
 	private List<TopLevelItem> all;
-	private List<TopLevelItem> added;
-	private List<TopLevelItem> filtered;
 
 	@Before
-	public void setUp() throws Exception {
-		super.setUp();
-		all = j.getInstance().getItems();
-		assertFalse("Jenkins jobs are required to run these tests", all.isEmpty());
+	public void before() throws Exception {
+	    all = Lists.<TopLevelItem>newArrayList(
+			createFreeStyleProject("job-1"),
+			createFreeStyleProject("job-2"),
+			createFreeStyleProject("job-3")
+		);
 	}
 
 	@Test
 	public void testShouldAddAllJobsWhenNoJobsPresent() throws Exception {
-		filter = new AddRemoveFallbackFilter(FallbackTypes.ADD_ALL_IF_NONE_INCLUDED.toString());
-		added = Lists.newArrayList();
-		filtered = filter.filter(added, all, null);
+		ViewJobFilter filter = new AddRemoveFallbackFilter(ADD_ALL_IF_NONE_INCLUDED.name());
 
-		assertEquals("Expected ALL jobs to be in the filtered list", all, filtered);
+		List<TopLevelItem> added = newArrayList();
+
+		List<TopLevelItem> expected = newArrayList(all);
+		List<TopLevelItem> filtered = filter.filter(added, all, null);
+
+		assertThat("Expected ALL jobs to be in the filtered list", filtered, is(expected));
 	}
 
 	@Test
 	public void testShouldNotModifyFilteredListWhenSomeJobPresent() throws Exception {
-		filter = new AddRemoveFallbackFilter(FallbackTypes.ADD_ALL_IF_NONE_INCLUDED.toString());
-		added = Lists.newArrayList(j.getInstance().getItem("Job-1"));
-		filtered = filter.filter(added, all, null);
+		ViewJobFilter filter = new AddRemoveFallbackFilter(ADD_ALL_IF_NONE_INCLUDED.name());
 
-		List<TopLevelItem> expected = Lists.newArrayList(j.getInstance().getItem("Job-1"));
-		assertEquals("Expected only Job-1 to be in the filtered list", expected, filtered);
+		List<TopLevelItem> added = newArrayList(getItem("job-1"));
+
+		List<TopLevelItem> expected = newArrayList(added);
+		List<TopLevelItem> filtered = filter.filter(added, all, null);
+
+		assertThat("Expected only job-1 to be in the filtered list", filtered, is(expected));
 	}
 
 	@Test
 	public void testShouldRemoveAllJobsWhenAllJobsPresent() throws Exception {
-		filter = new AddRemoveFallbackFilter(FallbackTypes.REMOVE_ALL_IF_ALL_INCLUDED.toString());
-		added = j.getInstance().getItems();
-		filtered = filter.filter(added, all, null);
+		ViewJobFilter filter = new AddRemoveFallbackFilter(REMOVE_ALL_IF_ALL_INCLUDED.name());
 
-		assertTrue("Expected NO jobs to be in the filtered list", filtered.isEmpty());
+		List<TopLevelItem> added = newArrayList(all);
+
+		List<TopLevelItem> expected = newArrayList();
+		List<TopLevelItem> filtered = filter.filter(added, all, null);
+
+		assertThat("Expected NO jobs to be in the filtered list", filtered, is(expected));
 	}
 
 	@Test
 	public void testShouldNotModifyFilteredListWhenAllJobsNotPresent() throws Exception {
-		filter = new AddRemoveFallbackFilter(FallbackTypes.REMOVE_ALL_IF_ALL_INCLUDED.toString());
-		added = Lists.newArrayList(j.getInstance().getItem("Job-1"));
-		filtered = filter.filter(added, all, null);
+		ViewJobFilter filter = new AddRemoveFallbackFilter(REMOVE_ALL_IF_ALL_INCLUDED.name());
 
-		List<TopLevelItem> expected = Lists.newArrayList(j.getInstance().getItem("Job-1"));
-		assertEquals("Expected only Job-1 to be in the filtered list", expected, filtered);
+		List<TopLevelItem> added = newArrayList(getItem("job-1"));
+
+		List<TopLevelItem> expected = newArrayList(added);
+		List<TopLevelItem> filtered = filter.filter(added, all, null);
+
+		assertEquals("Expected only job-1 to be in the filtered list", expected, filtered);
 	}
 
+	@Test
+	public void testConfigRoundtrip() throws Exception {
+		testConfigRoundtrip(
+			"view-1",
+			new AddRemoveFallbackFilter(ADD_ALL_IF_NONE_INCLUDED.name())
+		);
+
+		testConfigRoundtrip(
+			"view-2",
+			new AddRemoveFallbackFilter(REMOVE_ALL_IF_ALL_INCLUDED.name())
+		);
+
+		testConfigRoundtrip(
+			"view-3",
+			new AddRemoveFallbackFilter(ADD_ALL_IF_NONE_INCLUDED.name()),
+			new AddRemoveFallbackFilter(REMOVE_ALL_IF_ALL_INCLUDED.name())
+		);
+	}
+
+	private void testConfigRoundtrip(String viewName, AddRemoveFallbackFilter... filters) throws Exception {
+		List<AddRemoveFallbackFilter> expectedFilters = new ArrayList<AddRemoveFallbackFilter>();
+		for (AddRemoveFallbackFilter filter: filters) {
+			expectedFilters.add(new AddRemoveFallbackFilter(filter.getFallbackTypeString()));
+		}
+
+		ListView view = createFilteredView(viewName, filters);
+		j.configRoundtrip(view);
+
+		ListView viewAfterRoundtrip = (ListView)j.getInstance().getView(viewName);
+		assertFilterEquals(expectedFilters, viewAfterRoundtrip.getJobFilters());
+
+		viewAfterRoundtrip.save();
+		j.getInstance().reload();
+
+		ListView viewAfterReload = (ListView)j.getInstance().getView(viewName);
+		assertFilterEquals(expectedFilters, viewAfterReload.getJobFilters());
+	}
+
+	private void assertFilterEquals(List<AddRemoveFallbackFilter> expectedFilters, List<ViewJobFilter> actualFilters) {
+		assertThat(actualFilters.size(), is(expectedFilters.size()));
+		for (int i = 0; i < actualFilters.size(); i++) {
+			ViewJobFilter actualFilter = actualFilters.get(i);
+			AddRemoveFallbackFilter expectedFilter = expectedFilters.get(i);
+			assertThat(actualFilter, instanceOf(AddRemoveFallbackFilter.class));
+			assertThat(((AddRemoveFallbackFilter)actualFilter).getFallbackTypeString(), is(expectedFilter.getFallbackTypeString()));
+		}
+	}
 }
