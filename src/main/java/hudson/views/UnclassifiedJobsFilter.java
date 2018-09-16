145a469bca9c821f0c3fbf0fe98641a925653bfd
Sven Schoenung <sven.schoenung@gmail.com>
2018-09-16 11:14:00 +0200

    Fix StackOverflowException in UnclassifiedJobsFilter.
    When there's at least two views with an UnclassifiedJobsFilter then
    each view needs to call the other views getItems() method to figure
    out which jobs it contains. This leads to an infinite loop.
    
    There's nothing we can really do about this other than just ignore
    other views that contain an UnclassifiedJobsFilter.

--- src/main/java/hudson/views/UnclassifiedJobsFilter.java
+++ src/main/java/hudson/views/UnclassifiedJobsFilter.java
@@ -2,6 +2,7 @@ package hudson.views;
 
 import hudson.Extension;
 import hudson.model.Descriptor;
+import hudson.model.ListView;
 import hudson.model.TopLevelItem;
 import hudson.model.View;
 
@@ -12,7 +13,8 @@ import java.util.List;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
- * Returns all jobs that don't show up in other jobs, not counting any "all jobs" views.
+ * Returns all jobs that don't show up in other jobs, not counting any "all jobs" views
+ * and views that contain UnclassifiedJobsFilters.
  * @author jacob
  */
 public class UnclassifiedJobsFilter extends AbstractIncludeExcludeJobFilter {
@@ -25,25 +27,43 @@ public class UnclassifiedJobsFilter extends AbstractIncludeExcludeJobFilter {
     @Override
     protected void doFilter(List<TopLevelItem> filtered, List<TopLevelItem> all, View filteringView) {
     	List<View> allViews = OtherViewsFilter.getAllViews();
-		allViews.remove(filteringView);
-		
+    	List<View> allClassifyingViews = new ArrayList<View>(allViews.size());
+    	for (View view : allViews) {
+    		if (view != filteringView && !containsUnclassifiedJobsFilter(view)) {
+				allClassifyingViews.add(view);
+			}
+		}
+
 		int allJobsCount = all.size();
-		List<TopLevelItem> classified = getAllClassifiedItems(allViews, allJobsCount);
+		List<TopLevelItem> classified = getAllClassifiedItems(allClassifyingViews, allJobsCount);
 		
         for (TopLevelItem item: all) {
         	boolean matched = !classified.contains(item);
     		filterItem(filtered, item, matched);
         }
     }
-    private List<TopLevelItem> getAllClassifiedItems(List<View> allViews, int allJobsCount) {
+
+	private boolean containsUnclassifiedJobsFilter(View view) {
+	    if (view instanceof ListView) {
+	    	ListView listView = (ListView)view;
+	    	for (ViewJobFilter filter: listView.getJobFilters()) {
+	    		if (filter instanceof UnclassifiedJobsFilter) {
+	    			return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private List<TopLevelItem> getAllClassifiedItems(List<View> allViews, int allJobsCount) {
     	List<TopLevelItem> classified = new ArrayList<TopLevelItem>();
 		for (View otherView: allViews) {
 			Collection<TopLevelItem> items = otherView.getItems();
-			// do not bother looking at views that already contain all items
-			// the advantage of using this strategy is that it covers any type of "All Jobs" views
-			//		I care about this, because I have an AllJobsFilter, but anyone could have set up an ".*" regex, etc
-			// the disadvantage is that if some view just happens to cover all views (perhaps it happens over time)
-			//		then suddenly this filter stops accounting for that view
+			// Do not bother looking at views that already contain all items.
+			// The advantage of using this strategy is that it covers any type of "All Jobs" views.
+			// I care about this, because I have an AllJobsFilter, but anyone could have set up an ".*" regex, etc.
+			// The disadvantage is that if some view just happens to cover all views (perhaps it happens over time)
+			// then suddenly this filter stops accounting for that view
 			if (items.size() < allJobsCount) {
 				classified.addAll(items);
 			}
