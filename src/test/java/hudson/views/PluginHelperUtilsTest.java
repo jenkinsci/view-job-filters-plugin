f2bbd7f0fb57fd4be0a93ead8a50583b5d9ffd52
Sven Schoenung <sven.schoenung@gmail.com>
2018-09-19 22:10:03 +0200

    Improve PluginHelperUtilsTest

--- src/test/java/hudson/views/PluginHelperUtilsTest.java
+++ src/test/java/hudson/views/PluginHelperUtilsTest.java
@@ -2,6 +2,9 @@ package hudson.views;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class PluginHelperUtilsTest extends AbstractHudsonTest {
     /*
      * Test all the helpers to see that no exceptions are thrown.
@@ -16,5 +19,20 @@ public class PluginHelperUtilsTest extends AbstractHudsonTest {
         PluginHelperUtils.validateAndThrow(new MavenExtraStepsValuesHelper());
         PluginHelperUtils.validateAndThrow(new MavenProjectValuesHelper());
         PluginHelperUtils.validateAndThrow(new SvnValuesProvider());
+    }
+
+    @Test
+    public void testNull() {
+        assertThat(PluginHelperUtils.validateAndThrow(null), is(nullValue()));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testThrow() {
+        PluginHelperUtils.validateAndThrow(new PluginHelperUtils.PluginHelperTestable() {
+            @Override
+            public Class getPluginTesterClass() {
+                throw new RuntimeException("Test");
+            }
+        });
     }
 }
