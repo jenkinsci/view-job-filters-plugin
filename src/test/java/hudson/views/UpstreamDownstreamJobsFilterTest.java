4131b3d6e16cada098a10ec94413103d037e4c58
Sven Schoenung <sven.schoenung@gmail.com>
2018-10-09 22:32:19 +0200

    Add config roundtrip tests in UpstreamDownstreamJobsFilterTest

--- src/test/java/hudson/views/UpstreamDownstreamJobsFilterTest.java
+++ src/test/java/hudson/views/UpstreamDownstreamJobsFilterTest.java
@@ -1,6 +1,7 @@
 package hudson.views;
 
 import hudson.model.FreeStyleProject;
+import hudson.model.ListView;
 import hudson.model.TopLevelItem;
 import hudson.model.View;
 import hudson.views.test.JobMocker;
@@ -10,12 +11,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static hudson.views.AbstractBuildTrendFilter.AmountType.Builds;
+import static hudson.views.AbstractBuildTrendFilter.AmountType.Days;
+import static hudson.views.AbstractBuildTrendFilter.AmountType.Hours;
+import static hudson.views.AbstractBuildTrendFilter.BuildCountType.All;
+import static hudson.views.AbstractBuildTrendFilter.BuildCountType.AtLeastOne;
+import static hudson.views.AbstractBuildTrendFilter.BuildCountType.Latest;
+import static hudson.views.AbstractIncludeExcludeJobFilter.IncludeExcludeType.excludeMatched;
+import static hudson.views.AbstractIncludeExcludeJobFilter.IncludeExcludeType.includeMatched;
+import static hudson.views.AbstractIncludeExcludeJobFilter.IncludeExcludeType.includeUnmatched;
 import static hudson.views.test.JobMocker.freeStyleProject;
 import static hudson.views.test.ViewJobFilters.downstream;
 import static hudson.views.test.ViewJobFilters.upstream;
 import static hudson.views.test.ViewJobFilters.upstreamDownstream;
 import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
@@ -428,4 +439,54 @@ public class UpstreamDownstreamJobsFilterTest extends AbstractHudsonTest {
     private List<TopLevelItem> list(TopLevelItem... items) {
         return asList(items);
     }
+
+
+    @Test
+    public void testConfigRoundtrip() throws Exception {
+        testConfigRoundtrip(
+            "view-1",
+            new UpstreamDownstreamJobsFilter(true, false, true, false)
+        );
+
+        testConfigRoundtrip(
+            "view-2",
+            new UpstreamDownstreamJobsFilter(false, true, false, true),
+            new UpstreamDownstreamJobsFilter(false, false, true, true)
+        );
+
+    }
+
+    private void testConfigRoundtrip(String viewName, UpstreamDownstreamJobsFilter... filters) throws Exception {
+        List<UpstreamDownstreamJobsFilter> expectedFilters = new ArrayList<UpstreamDownstreamJobsFilter>();
+        for (UpstreamDownstreamJobsFilter filter : filters) {
+            expectedFilters.add(new UpstreamDownstreamJobsFilter(
+                filter.isIncludeDownstream(), filter.isIncludeUpstream(), filter.isRecursive(), filter.isExcludeOriginals()));
+        }
+
+        ListView view = createFilteredView(viewName, filters);
+        j.configRoundtrip(view);
+
+        ListView viewAfterRoundtrip = (ListView) j.getInstance().getView(viewName);
+        assertFilterEquals(expectedFilters, viewAfterRoundtrip.getJobFilters());
+
+        viewAfterRoundtrip.save();
+        j.getInstance().reload();
+
+        ListView viewAfterReload = (ListView) j.getInstance().getView(viewName);
+        assertFilterEquals(expectedFilters, viewAfterReload.getJobFilters());
+    }
+
+    private void assertFilterEquals(List<UpstreamDownstreamJobsFilter> expectedFilters, List<ViewJobFilter> actualFilters) {
+        assertThat(actualFilters.size(), is(expectedFilters.size()));
+        for (int i = 0; i < actualFilters.size(); i++) {
+            ViewJobFilter actualFilter = actualFilters.get(i);
+            UpstreamDownstreamJobsFilter expectedFilter = expectedFilters.get(i);
+            assertThat(actualFilter, instanceOf(UpstreamDownstreamJobsFilter.class));
+            assertThat(((UpstreamDownstreamJobsFilter)actualFilter).isIncludeDownstream(), is(expectedFilter.isIncludeDownstream()));
+            assertThat(((UpstreamDownstreamJobsFilter)actualFilter).isIncludeUpstream(), is(expectedFilter.isIncludeUpstream()));
+            assertThat(((UpstreamDownstreamJobsFilter)actualFilter).isRecursive(), is(expectedFilter.isRecursive()));
+            assertThat(((UpstreamDownstreamJobsFilter)actualFilter).isExcludeOriginals(), is(expectedFilter.isExcludeOriginals()));
+        }
+    }
+
 }
