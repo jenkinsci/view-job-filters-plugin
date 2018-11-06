5ea5b7820364c8b4c700d22d4ecf50a268e0a737
Sven Schoenung <sven.schoenung@gmail.com>
2018-11-06 18:31:17 +0100

    Remove unnecessary methods from AbstractJenkinsTest

--- src/test/java/hudson/views/AbstractJenkinsTest.java
+++ src/test/java/hudson/views/AbstractJenkinsTest.java
@@ -31,25 +31,6 @@ public abstract class AbstractJenkinsTest {
 	@Rule
 	public JenkinsRule j = new JenkinsRule();
 
-	public void setUp() throws Exception {
-		// create some jobs
-		for (int i = 0; i < 10; i++) {
-			j.createFreeStyleProject("Job-" + i);
-		}
-		addRegexView("View-56", "Job.*[56]");
-		addRegexView("View-13", "Job.*[13]");
-	}
-
-	private void addRegexView(String name, String regex) throws IOException {
-		ListView v = new ListView(name);
-		DescribableList<ViewJobFilter, Descriptor<ViewJobFilter>> filters = v.getJobFilters();
-		RegExJobFilter regexFilter = new RegExJobFilter(regex, 
-				AbstractIncludeExcludeJobFilter.IncludeExcludeType.includeMatched.toString(),
-				RegExJobFilter.ValueType.NAME.toString());
-		filters.add(regexFilter);
-		j.getInstance().addView(v);
-	}
-
     protected ListView createFilteredView(String name, ViewJobFilter... filters) throws IOException {
         ListView view = new ListView(name, j.getInstance());
         for (ViewJobFilter filter: filters) {
