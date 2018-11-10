3b73759afafc72590c065d208699ce6fe5da9c98
Sven Schoenung <sven.schoenung@gmail.com>
2018-11-10 20:38:57 +0100

    Fix build filter column for status and weather

--- src/main/java/hudson/views/BuildFilterColumn.java
+++ src/main/java/hudson/views/BuildFilterColumn.java
@@ -1,23 +1,26 @@
 package hudson.views;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.DescriptorExtensionList;
 import hudson.Extension;
-import hudson.model.RunMap;
-import hudson.model.Descriptor;
-import hudson.model.Hudson;
-import hudson.model.Job;
-import hudson.model.ListView;
-import hudson.model.Run;
-import hudson.model.View;
+import hudson.model.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.SortedMap;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.logging.Logger;
 
+import hudson.util.RunList;
+import javassist.util.proxy.MethodHandler;
+import javassist.util.proxy.ProxyFactory;
 import net.sf.json.JSONObject;
 
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
 
 /**
  * This column wraps another column so that when that delegate column renders, it is
@@ -27,12 +30,19 @@ import org.kohsuke.stapler.StaplerRequest;
  */
 public class BuildFilterColumn extends ListViewColumn {
 
-	private ListViewColumn delegate;
+    private static final Logger LOGGER = Logger.getLogger(BuildFilterColumn.class.getName());
+
+    private ListViewColumn delegate;
 	private ListView view;
 
 	@DataBoundConstructor
 	public BuildFilterColumn(ListViewColumn delegate) {
 		this.delegate = delegate;
+	}
+
+	public BuildFilterColumn(ListViewColumn delegate, ListView view) {
+		this(delegate);
+		this.view = view;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -97,46 +107,124 @@ public class BuildFilterColumn extends ListViewColumn {
 			return false;
 		}
 	}
-	@SuppressWarnings("unchecked")
-	public Job getJobWrapper(Job job) {
-		return new JobWrapper(job);
-	}
+
 	public ListViewColumn getDelegate() {
 		return delegate;
 	}
-	
-	@SuppressWarnings("unchecked")
-	class JobWrapper extends Job {
 
-		private Job delegate;
-		
+	@SuppressWarnings("unchecked")
+	public Job getJobWrapper(final Job job) {
+
+		final JobWrapper wrapper = new JobWrapper(job);
+
+		try {
+			ProxyFactory factory = new ProxyFactory();
+			factory.setSuperclass(job.getClass());
+
+			MethodHandler handler = new MethodHandler() {
+				@Override
+				public Object invoke(Object self, Method thisMethod, Method proceed, Object[] args) throws Throwable {
+					if (matches(thisMethod, args, "_getRuns") && thisMethod.getReturnType().equals(SortedMap.class)) {
+						return wrapper._getRuns();
+					}
+					if (matches(thisMethod, args, "getBuilds")) {
+						return wrapper.getBuilds();
+					}
+					if (matches(thisMethod, args, "getLastBuild")) {
+						return wrapper.getLastBuild();
+					}
+					if (matches(thisMethod, args, "getIconColor")) {
+						return wrapper.getIconColor();
+					}
+					if (matches(thisMethod, args, "getBuildHealthReports")) {
+						return wrapper.getBuildHealthReports();
+					}
+					return thisMethod.invoke(job, args);
+				}
+			};
+			return (Job) factory.create(
+				new Class[] { ItemGroup.class, String.class },
+				new Object[] { job.getParent(), job.getName() },
+				handler
+			);
+		} catch (Exception e) {
+            LOGGER.info("Can't proxy job of type " + job.getClass() + ": " + e.getMessage());
+            LOGGER.info("Falling back to simple wrapper object");
+		    return wrapper;
+		}
+	}
+
+    @SuppressFBWarnings(value="REC_CATCH_EXCEPTION")
+	public Run getRunWrapper(final Run run) {
+		final RunWrapper wrapper = new RunWrapper(run);
+
+		try {
+			ProxyFactory factory = new ProxyFactory();
+			factory.setSuperclass(run.getClass());
+
+			MethodHandler handler = new MethodHandler() {
+				@Override
+				public Object invoke(Object self, Method thisMethod, Method proceed, Object[] args) throws Throwable {
+				   	if (matches(thisMethod, args, "_this")) {
+						return wrapper._this();
+					}
+					if (matches(thisMethod, args, "getNextBuild")) {
+						return wrapper.getNextBuild();
+					}
+					if (matches(thisMethod, args, "getPreviousBuild")) {
+						return wrapper.getPreviousBuild();
+					}
+					return thisMethod.invoke(run, args);
+				}
+			};
+			return (Run) factory.create(
+					new Class[] { run.getParent().getClass() },
+					new Object[] { run.getParent() },
+					handler
+			);
+		} catch (Exception e) {
+            LOGGER.info("Can't proxy build of type " + run.getClass() + ": " + e.getMessage());
+            LOGGER.info("Falling back to simple wrapper object");
+			return wrapper;
+		}
+	}
+
+	public class JobWrapper extends Job {
+		private final Job delegate;
+
 		public JobWrapper(Job delegate) {
 			super(delegate.getParent(), delegate.getName());
 			this.delegate = delegate;
 		}
 
 		@Override
-		protected SortedMap _getRuns() {
-			RunMap builds = new RunMap();
-			SortedMap map = delegate.getBuildsAsMap();
-			for (Object runObject: map.values()) {
-				Run run = (Run) runObject;
-				if (matchesRun(run)) {
-					builds.put(run);
-				}
+
+		public Run getLastBuild() {
+            SortedMap<Integer, Run> runs = _getRuns();
+            Integer id = runs.lastKey();
+			if (id != null) {
+				return runs.get(id);
 			}
-			return builds;
+			return null;
 		}
-		public boolean matchesRun(Run run) {
-			for (ViewJobFilter filter: view.getJobFilters()) {
-				if (filter instanceof RunMatcher) {
-					RunMatcher matcher = (RunMatcher) filter;
-					if (!matcher.matchesRun(run)) {
-						return false;
-					}
+
+		@Override
+		public RunList getBuilds() {
+			return RunList.fromRuns(_getRuns().values());
+		}
+
+		@Override
+		protected SortedMap<Integer, Run> _getRuns() {
+			SortedMap<Integer, Run> builds = new TreeMap<Integer, Run>();
+
+			SortedMap<Integer, Run> buildsAsMap = delegate.getBuildsAsMap();
+			for (Map.Entry<Integer, Run> entry : buildsAsMap.entrySet()) {
+				if (matchesRun(entry.getValue())) {
+					builds.put(entry.getKey(), getRunWrapper(entry.getValue()));
 				}
 			}
-			return true;
+
+			return builds;
 		}
 
 		@Override
@@ -146,9 +234,71 @@ public class BuildFilterColumn extends ListViewColumn {
 
 		@Override
 		protected void removeRun(Run run) {
-			// can't do this - it's protected
-//			delegate.removeRun(run);
 		}
 	}
-	
+
+	public class RunWrapper extends Run {
+		private final Run delegate;
+
+		public RunWrapper(Run delegate) {
+			super(delegate.getParent(), delegate.getTimestamp());
+			this.delegate = delegate;
+		}
+
+		@Nonnull
+		@Override
+		protected Run _this() {
+			return this;
+		}
+
+		@CheckForNull
+		@Override
+		public Run getPreviousBuild() {
+			for (Run i = delegate.getPreviousBuild(); i != null; i = i.getPreviousBuild()) {
+			    if (matchesRun(i)) {
+			        return getRunWrapper(i);
+                }
+            }
+			return null;
+		}
+
+		@CheckForNull
+		@Override
+		public Run getNextBuild() {
+  			for (Run i = delegate.getNextBuild(); i != null; i = i.getNextBuild()) {
+			    if (matchesRun(i)) {
+			        return getRunWrapper(i);
+                }
+            }
+            return null;
+		}
+	}
+
+	private boolean matches(Method method, Object[] args, String name, Class... argTypes) {
+		if (!method.getName().equals(name)) {
+			return false;
+		}
+		if (args.length != argTypes.length) {
+			return false;
+		}
+		for (int i = 0; i < argTypes.length; i++) {
+			if (!argTypes[i].isInstance(args[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+    private boolean matchesRun(Run run) {
+        for (ViewJobFilter filter: view.getJobFilters()) {
+            if (filter instanceof RunMatcher) {
+                RunMatcher matcher = (RunMatcher) filter;
+                if (!matcher.matchesRun(run)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
---
+++ src/test/java/hudson/views/BuildFilterColumnTest.java
@@ -0,0 +1,100 @@
+package hudson.views;
+
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import hudson.Functions;
+import hudson.model.*;
+import hudson.tasks.BatchFile;
+import hudson.tasks.Shell;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
+import static hudson.views.AbstractIncludeExcludeJobFilter.IncludeExcludeType.includeMatched;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+public class BuildFilterColumnTest extends AbstractJenkinsTest {
+
+    @Test
+    public void testWrappedStatusColumn() throws Exception {
+        FreeStyleProject project = createFreeStyleProject("project");
+
+        ListView view = createViewWithBuildFilterColumn(new StatusColumn());
+
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "master");
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "test");
+
+        assertThat(getBuildFilterColumn(view).querySelector("img").getAttributes().getNamedItem("class").getTextContent(), containsString("icon-blue"));
+
+        runWithParameter(project, Result.FAILURE, "BRANCH", "master");
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "test");
+
+        assertThat(getBuildFilterColumn(view).querySelector("img").getAttributes().getNamedItem("class").getTextContent(), containsString("icon-red"));
+
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "master");
+        runWithParameter(project, Result.FAILURE, "BRANCH", "test");
+
+        assertThat(getBuildFilterColumn(view).querySelector("img").getAttributes().getNamedItem("class").getTextContent(), containsString("icon-blue"));
+    }
+
+    @Test
+    public void testWrappedWeatherColumn() throws Exception {
+        FreeStyleProject project = createFreeStyleProject("project");
+
+        ListView view = createViewWithBuildFilterColumn(new WeatherColumn());
+
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "test");
+        runWithParameter(project, Result.FAILURE, "BRANCH", "master");
+
+        assertThat(getBuildFilterColumn(view).querySelector("img").getAttributes().getNamedItem("class").getTextContent(), containsString("00to19"));
+
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "test");
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "master");
+
+        assertThat(getBuildFilterColumn(view).querySelector("img").getAttributes().getNamedItem("class").getTextContent(), containsString("40to59"));
+
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "test");
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "master");
+
+        assertThat(getBuildFilterColumn(view).querySelector("img").getAttributes().getNamedItem("class").getTextContent(), containsString("60to79"));
+
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "test");
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "master");
+
+        assertThat(getBuildFilterColumn(view).querySelector("img").getAttributes().getNamedItem("class").getTextContent(), containsString("60to79"));
+
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "test");
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "master");
+
+        assertThat(getBuildFilterColumn(view).querySelector("img").getAttributes().getNamedItem("class").getTextContent(), containsString("60to79"));
+
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "test");
+        runWithParameter(project, Result.SUCCESS, "BRANCH", "master");
+
+        assertThat(getBuildFilterColumn(view).querySelector("img").getAttributes().getNamedItem("class").getTextContent(), containsString("80plus"));
+    }
+
+    private FreeStyleBuild runWithParameter(FreeStyleProject project, Result result, String paramName, String paramValue) throws Exception {
+        String command = "exit " + ((result == Result.SUCCESS) ? 0 : 1);
+        project.getBuildersList().clear();
+        project.getBuildersList().add(Functions.isWindows() ? new BatchFile(command) : new Shell(command));
+        return j.assertBuildStatus(result, project.scheduleBuild2(0, (Cause)null, new ParametersAction(new StringParameterValue(paramName, paramValue))));
+    }
+
+    private ListView createViewWithBuildFilterColumn(ListViewColumn column) throws IOException {
+        ListView view = createFilteredView("view", new ParameterFilter(includeMatched.name(),
+                "BRANCH", "master", "",
+                false, true, 0, false));
+        view.getColumns().clear();
+        view.getColumns().add(new BuildFilterColumn(column, view));
+        j.getInstance().addView(view);
+        return view;
+    }
+
+    private DomNode getBuildFilterColumn(ListView view) throws IOException, SAXException {
+        HtmlPage viewPage = j.createWebClient().getPage(view);
+        return viewPage.querySelectorAll(".bigtable > tbody > tr > td").get(0);
+    }
+}
