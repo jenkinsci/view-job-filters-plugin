759a5bc4995dfbd1f71e9044f9202fc22ce18199
Kenneth Rogers <kerogers@cloudbees.com>
2020-06-16 16:53:30 -0400

    [CPLT2-6401] Changed 24.0 to 24.0F in the configuration-as-code.yaml to make the test pass. Added additional testing of the configured view.

--- src/test/java/hudson/views/ViewJobFiltersConfigAsCodeTest.java
+++ src/test/java/hudson/views/ViewJobFiltersConfigAsCodeTest.java
@@ -1,5 +1,6 @@
 package hudson.views;
 
+import hudson.model.AllView;
 import hudson.model.ListView;
 import io.jenkins.plugins.casc.misc.RoundTripAbstractTest;
 import org.jvnet.hudson.test.RestartableJenkinsRule;
@@ -12,12 +13,23 @@ public class ViewJobFiltersConfigAsCodeTest extends RoundTripAbstractTest {
     @Override
     protected void assertConfiguredAsExpected(RestartableJenkinsRule restartableJenkinsRule, String s) {
         assertThat(r.j.getInstance().getViews().size(), is(2));
+        AllView allView = (AllView) r.j.getInstance().getView("all");
+        assertThat(allView, notNullValue());
         ListView listView = (ListView) r.j.getInstance().getView("test");
         assertThat(listView, notNullValue());
         assertThat(listView.getIncludeRegex(), is(".*"));
         assertThat(listView.getDescription(), is("test view"));
         assertThat(listView.getJobFilters().get(0), is(instanceOf(BuildDurationFilter.class)));
-
+        BuildDurationFilter buildDurationFilter = (BuildDurationFilter) listView.getJobFilters().get(0);
+        assertThat( buildDurationFilter.getAmount(), is(24.0F));
+        assertThat(buildDurationFilter.getAmountTypeString(), is("Hours"));
+        assertThat(buildDurationFilter.isLessThan(), is(true));
+        assertThat(buildDurationFilter.getBuildDurationMinutes(), is("10"));
+        assertThat(buildDurationFilter.getBuildCountTypeString(), is("Latest"));
+        assertThat(buildDurationFilter.getIncludeExcludeTypeString(), is("includeMatched"));
+        assertThat(listView.getColumns().size(), is(7));
+        assertThat(listView.getColumns().get(0).getDescriptor().getDisplayName(), is("Status"));
+        assertThat(listView.getColumns().get(3).getDescriptor().getDisplayName(), is("Last Success"));
     }
 
     @Override
